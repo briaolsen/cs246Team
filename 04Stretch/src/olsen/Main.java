5cdbee231eb93b40389df5d93aaa9641a079b502
Briana Olsen <bria.olsen@gmail.com>
2020-05-14 20:22:44 -0600

    Second change

--- 04Stretch/src/olsen/Main.java
+++ 04Stretch/src/olsen/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("This is testing the rollback and reset feature");
         System.out.println("First change for second commit");
+        System.out.println("Second change for third commit");
     }
 }
